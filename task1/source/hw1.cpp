c7b1cd48220c91b5d0bf32ec433d6189dff439c6
xMET0x <st102774@student.spbu.ru>
2021-10-08 23:34:38 +0300

    add one task

---
+++ task1/source/hw1.cpp
@@ -0,0 +1,51 @@
+﻿// hw1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() 
+{
+    /*int n;
+    std::cout << "N";
+    do
+    {
+        std::cin >> n;
+    } while (n < 0);
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < i; j++)
+        {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }*/
+    /*int k;
+    std::cout << "PiromidOchca";
+    do
+    {
+        std::cin >> k;
+    } while (k < 0);
+    for (int i = 1; i <= k; ++i) {
+        for (int j = 0; i <= k; ++j) {
+
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }*/
+    /*int p;
+    std::cout << "3ex";
+    do
+    {
+        std::cin >> p;
+    } while (p < 0);
+    for (int i = 1; i <= p; ++i) 
+    {
+        for (int j = 0; j <   p-i; ++j) {
+                std::cout << " ";
+        }
+        for (int k = 1; k <= i; ++k) {
+                std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+    */
+}
